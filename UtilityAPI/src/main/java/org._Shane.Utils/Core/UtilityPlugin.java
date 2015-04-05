4d2b2dd25fe6d52c4ef3df693bf72e0d7e8fc8dd
_Shane <mcphillygin@gmail.com>
2015-04-05 01:32:12 -0400

    Update UtilityPlugin.java

--- UtilityAPI/src/main/java/org._Shane.Utils/Core/UtilityPlugin.java
+++ UtilityAPI/src/main/java/org._Shane.Utils/Core/UtilityPlugin.java
@@ -1,1 +1,22 @@
+package org._Shane.Utils.Core;
 
+import Utils.ListiningEvents.Events.Events;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UtilityPlugin extends JavaPlugin
+{
+    public static Plugin plugin;
+    public void onEnable()
+    {
+        plugin = this; //First thing to be enabled.
+        this.getServer().getPluginManager().registerEvents(new Events(), this);
+    }
+
+    public void onDisable()
+    {
+        Bukkit.getScheduler().cancelTasks(plugin);
+        plugin = null; //Last thing to be disabled.
+    }
+}
