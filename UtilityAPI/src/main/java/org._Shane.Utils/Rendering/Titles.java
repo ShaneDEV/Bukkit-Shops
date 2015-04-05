ea3cc6a9920cb4184619e04af748c12916cf0c9e
_Shane <mcphillygin@gmail.com>
2015-04-05 13:02:19 -0400

    Update Titles.java

--- UtilityAPI/src/main/java/org._Shane.Utils/Rendering/Titles.java
+++ UtilityAPI/src/main/java/org._Shane.Utils/Rendering/Titles.java
@@ -1,5 +1,6 @@
 package org._Shane.Utils.Rendering;
 
+import org._Shane.Utils.Other.Misc;
 import org._Shane.Utils.ReflectionResource.APIReflection;
 import org.bukkit.entity.Player;
 
@@ -8,25 +9,32 @@ import java.lang.reflect.Method;
 
 public class Titles
 {
-    /* Available title actions.
-        TITLE
-        SUBTITLE
-        TIMES
-        CLEAR
-        RESET */
+    public enum Actions
+    {
+        TITLE("TITLE"),
+        SUBTITLE("SUBTITLE"),
+        TIMES("TIMES"),
+        CLEAR("CLEAR"),
+        RESET("RESET");
 
+        private String name;
+        private Actions(String name)
+        { this.name = name; }
 
+        public String getName()
+        { return name; }
+    }
 
     /**
      *
      * @param player Player to send packet to.
      * @param message Message to be rendered when packet is successfully sent.
-     * @param type Type to declare Title Actions.
+     * @param action value of Actions enum for Titles/Subtitles.
      * @param fadeIn Time (Millis) to fade in.
      * @param stay Time (Millis) to stay rendered.
      * @param fadeOut Time (Millis) to fade out.
      */
-    public static void sendTitleAction(Player player, String message, String type, int fadeIn, int stay, int fadeOut)
+    public static void sendTitleAction(Player player, String message, Actions action, int fadeIn, int stay, int fadeOut)
     {
         try {
             Class<?> IChatBaseComponent = APIReflection.getNMS("IChatBaseComponent");
@@ -34,11 +42,12 @@ public class Titles
             Class<?> ChatSerializer = APIReflection.getNMS("ChatSerializer");
             Class<?> ETitleAction = APIReflection.getNMS("EnumTitleAction");
             Method aChatSerializer = ChatSerializer.getDeclaredMethod("a", new Class[] { String.class });
-            Field f = ETitleAction.getDeclaredField(type);
-            f.setAccessible(true);
 
-            Object o = aChatSerializer.invoke(null, new Object[] {message});
-            Object packet = PacketPlayOutTitle.getConstructor(new Class[] {ETitleAction, IChatBaseComponent, Integer.TYPE, Integer.TYPE, Integer.TYPE}).newInstance(f.get(ETitleAction), IChatBaseComponent.cast(o), Integer.valueOf(fadeIn), Integer.valueOf(stay), Integer.valueOf(fadeOut));
+            Field titleAction = ETitleAction.getDeclaredField(action.getName());
+            titleAction.setAccessible(true);
+
+            Object o = aChatSerializer.invoke(null, new Object[] {Misc.colourize("{\"text\": \"" + message + "\"}")});
+            Object packet = PacketPlayOutTitle.getConstructor(new Class[] {ETitleAction, IChatBaseComponent, Integer.TYPE, Integer.TYPE, Integer.TYPE}).newInstance(titleAction.get(ETitleAction), IChatBaseComponent.cast(o), Integer.valueOf(fadeIn), Integer.valueOf(stay), Integer.valueOf(fadeOut));
             APIReflection.sendPacket(player, packet);
         } catch (Exception e)
         {
@@ -46,5 +55,3 @@ public class Titles
         }
     }
 }
-
-
