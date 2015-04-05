3eeb968215572a6d3fc20625d1c92bb7528441ed
_Shane <mcphillygin@gmail.com>
2015-04-05 13:04:09 -0400

    Added tab testing. Working :)

--- UtilityAPI/src/main/java/Utils.ListiningEvents/Events.java
+++ UtilityAPI/src/main/java/Utils.ListiningEvents/Events.java
@@ -1,5 +1,6 @@
 package Utils.ListiningEvents;
 
+import org._Shane.Utils.Rendering.Tab;
 import org._Shane.Utils.Rendering.Titles;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +11,8 @@ public class Events implements Listener
     @EventHandler
     public void onJoin(PlayerJoinEvent event)
     {
-        Titles.sendTitleAction(event.getPlayer(), "&c&lSERVER NAME", "TITLE", 5, 15, 5);
-        Titles.sendTitleAction(event.getPlayer(), "&f&l" + event.getPlayer().getName() + " &c&lwelcome to server!", "SUBTITLE", 5, 15, 5);
+        Titles.sendTitleAction(event.getPlayer(), "&c&lSERVER NAME", Titles.Actions.TITLE, 5, 15, 5);
+        Titles.sendTitleAction(event.getPlayer(), "&f&l" + event.getPlayer().getName() + " &c&lwelcome to server!", Titles.Actions.SUBTITLE, 5, 15, 5);
+        Tab.tab_Header_Footer(event.getPlayer(), "&c&lHello", "&a&lHello");
     }
 }
